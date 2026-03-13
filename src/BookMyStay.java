ef0580bb24cc5c61302a7de4316e4e41a66761a7
AryanSinha007 <aryansinha0216@gmail.com>
2026-03-13 10:10:03 +0530

    UC1

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,19 @@
-public class BookMyStay {
+import java.util.*;
+
+class BookMyStayApp{
+
+    public static void welcomeMessage(){
+        System.out.println("=================================================");
+        System.out.println("              WELCOME TO BOOK MY STAY            ");
+        System.out.println("=================================================");
+        System.out.println("Find and book the perfect stay for your trip!");
+        System.out.println();
+        System.out.println("Author: Aryan");
+        System.out.println("Version: 0.1");
+        System.out.println("-------------------------------------------------");
+
+    }
+    public static void main(String[] args){
+        welcomeMessage();
+    }
 }
