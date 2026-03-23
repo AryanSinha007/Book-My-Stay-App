306554fa6714dee61264dca01080fee008d40e0f
AryanSinha007 <aryansinha0216@gmail.com>
2026-03-23 10:59:36 +0530

    UC6

--- src/UseCase6RoomAllocation.java
+++ src/UseCase6RoomAllocation.java
@@ -1,5 +1,5 @@
 
-class UseCase5BookingRequestQueue {
+class UseCase6RoomAllocation {
 
     public static void welcomeMessage() {
         System.out.println("=================================================");
@@ -48,5 +48,20 @@ class UseCase5BookingRequestQueue {
 
         }
 
+        System.out.println("\nRoom Allocation Processing\n");
+
+        RoomAllocationService allocationService = new RoomAllocationService();
+
+        // Re-create queue again OR reuse logic
+        BookingRequestQueue bookingQueue2 = new BookingRequestQueue();
+
+        bookingQueue2.addRequest(new Reservation("Abhi", "SingleRoom"));
+        bookingQueue2.addRequest(new Reservation("Subha", "DoubleRoom"));
+        bookingQueue2.addRequest(new Reservation("Vanmathi", "SuiteRoom"));
+
+        while (bookingQueue2.hasPendingRequests()) {
+            Reservation r = bookingQueue2.getNextRequest();
+            allocationService.allocateRoom(r, inventory);
+        }
     }
 }
