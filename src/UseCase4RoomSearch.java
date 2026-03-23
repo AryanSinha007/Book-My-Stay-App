21164060c7e9b4db04496ebfdb0263eb3852e743
AryanSinha007 <aryansinha0216@gmail.com>
2026-03-23 10:47:19 +0530

    UC4

--- src/UseCase4RoomSearch.java
+++ src/UseCase4RoomSearch.java
@@ -1,24 +1,30 @@
-public class UseCase3RoomInitialization {
-    public static void welcomeMessage(){
+
+class UseCase4RoomSearch {
+
+    public static void welcomeMessage() {
         System.out.println("=================================================");
         System.out.println("              WELCOME TO BOOK MY STAY            ");
         System.out.println("=================================================");
         System.out.println("Find and book the perfect stay for your trip!");
         System.out.println();
         System.out.println("Author: Harshal");
-        System.out.println("Version: 3.1");
+        System.out.println("Version: 4.1");
         System.out.println("-------------------------------------------------");
         System.out.println("            Hotel Room Inventory Status");
 
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         welcomeMessage();
         SingleRoom SngR = new SingleRoom();
         DoubleRoom DbR = new DoubleRoom();
         SuiteRoom StR = new SuiteRoom();
+        RoomInventory inventory = new RoomInventory();
+        RoomSearchService searchService = new RoomSearchService();
 
-        SngR.displayRoomDetails();
-        DbR.displayRoomDetails();
-        StR.displayRoomDetails();
+        // SngR.displayRoomDetails();
+        // DbR.displayRoomDetails();
+        // StR.displayRoomDetails();
+        searchService.searchAvailableRooms(inventory, SngR, DbR, StR);
     }
 }
