3d55d7d8b01c572a8955d916727901f23ab110e2
AryanSinha007 <aryansinha0216@gmail.com>
2026-03-23 10:17:24 +0530

    UC2

---
+++ src/Room.java
@@ -0,0 +1,14 @@
+
+public abstract class Room{
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int nb, int sf, double pn){
+        this.numberOfBeds = nb;
+        this.squareFeet = sf;
+        this.pricePerNight = pn;
+    }
+
+    public abstract void displayRoomDetails();
+}
---
+++ src/UseCase3RoomInitialization.java
@@ -0,0 +1,24 @@
+public class UseCase3RoomInitialization {
+    public static void welcomeMessage(){
+        System.out.println("=================================================");
+        System.out.println("              WELCOME TO BOOK MY STAY            ");
+        System.out.println("=================================================");
+        System.out.println("Find and book the perfect stay for your trip!");
+        System.out.println();
+        System.out.println("Author: Harshal");
+        System.out.println("Version: 3.1");
+        System.out.println("-------------------------------------------------");
+        System.out.println("            Hotel Room Inventory Status");
+
+    }
+    public static void main(String[] args){
+        welcomeMessage();
+        SingleRoom SngR = new SingleRoom();
+        DoubleRoom DbR = new DoubleRoom();
+        SuiteRoom StR = new SuiteRoom();
+
+        SngR.displayRoomDetails();
+        DbR.displayRoomDetails();
+        StR.displayRoomDetails();
+    }
+}
